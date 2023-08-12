7b777556419f57db4ca13c597d8a1a33ae703a76
0xpetra <franco.petraz@gmail.com>
2023-08-12 20:25:25 +0200

    📦 NEW: fix typing

--- src/components/layout/NetworkStatus.tsx
+++ src/components/layout/NetworkStatus.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Badge, Flex, Text, useColorModeValue } from '@chakra-ui/react'
 import { useBlockNumber, useNetwork } from 'wagmi'
-import { GetNetworkColor } from 'utils/network'
+// import { GetNetworkColor } from 'utils/network'
 import { LinkComponent } from './LinkComponent'
 import { THEME_COLOR_SCHEME } from 'utils/config'
 
@@ -13,9 +13,9 @@ export function NetworkStatus() {
 
  return (
   <Flex alignItems="center" gap={2} zIndex={2} bgColor={bgColor} p={1}>
-   <Badge colorScheme={GetNetworkColor(network.chain?.network)} fontSize="2xs">
+   {/* <Badge colorScheme={GetNetworkColor(network.chain?.network)} fontSize="2xs">
     {network.chain?.name ?? 'Ethereum'}
-   </Badge>
+   </Badge> */}
    {explorerUrl && (
     <LinkComponent href={explorerUrl}>
      <Text fontSize="2xs"># {block.data?.toString()}</Text>
